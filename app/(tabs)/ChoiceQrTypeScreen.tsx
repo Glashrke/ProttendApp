fd1cb85c4e08600b1ed34c8ba42f264483a6bc29
Angel Rodriguez <angel.rodriguezleon@unisys.com>
2025-11-04 09:58:49 -0300

    Qr code prototipo

--- app/(tabs)/ChoiceQrTypeScreen.tsx
+++ app/(tabs)/ChoiceQrTypeScreen.tsx
@@ -1,11 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
 
+import { QRCode } from 'react-qr-svg';
 import BottomGlobalNavBar from "../../components/BottomGlobalNavBar";
 
 export default function ChoiceQrTypeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Pantalla de QR</Text>
+        <div style={styles.qrcode}>
+          <QRCode
+            level="Q"
+            style={{ width: 256 }}
+            value={JSON.stringify({//Estos campos se van a sacar de una query a la base de datos
+              id: 928328,
+              name: 'Jane Doe',
+              insider: true,
+            })}
+          />
+        </div>
       <BottomGlobalNavBar />
     </View>
   );
@@ -20,5 +32,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     textAlign: "center",
     marginTop: 50,
+  },qrcode: {
+    textAlign: 'center',
   },
 });
