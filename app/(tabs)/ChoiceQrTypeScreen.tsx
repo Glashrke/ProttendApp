8d7a07085ab92d27e463093a811a18c0c3397a28
Angel Rodriguez <angel.rodriguezleon@unisys.com>
2025-11-06 11:33:19 -0300

    Funcionalidad del qr

--- app/(tabs)/ChoiceQrTypeScreen.tsx
+++ app/(tabs)/ChoiceQrTypeScreen.tsx
@@ -1,23 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-import { QRCode } from 'react-qr-svg';
+import { QRCodeSVG } from 'qrcode.react';
 import BottomGlobalNavBar from "../../components/BottomGlobalNavBar";
 
+const imei = "356938035643809"; // reemplaza con el IMEI real
+const fechaHora = new Date().toISOString(); // formato ISO: "2025-11-06T14:32:00.000Z"
+
+const data = {
+  imei: imei,
+  timestamp: fechaHora
+};
+
+const jsonString = JSON.stringify(data);
 export default function ChoiceQrTypeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Pantalla de QR</Text>
-        <div style={styles.qrcode}>
-          <QRCode
-            level="Q"
-            style={{ width: 256 }}
-            value={JSON.stringify({//Estos campos se van a sacar de una query a la base de datos
-              id: 928328,
-              name: 'Jane Doe',
-              insider: true,
-            })}
-          />
-        </div>
+      <div style={styles.qrcode}>
+        <QRCodeSVG value={jsonString} size={256} level="H"/>
+      </div>
       <BottomGlobalNavBar />
     </View>
   );
